c205fc9f997965381ce81dd9536dcfb4dff6a77c
Alex Olivier <alex@alexolivier.me>
2018-03-11 20:51:43 +0000

    streaming

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// Imports the Google Cloud client library
+const net = require('net')
 const BigQuery = require('@google-cloud/bigquery')
-const axios = require('axios')
 
+const PIIP = '192.168.3.175'
+const PIPort = 10001
 const projectId = 'alex-olivier'
 const datasetId = 'flighttracker_dev'
-const tableId = 'locations'
+const tableId = 'aircraft_stream'
 
 // Creates a client
 const bigquery = new BigQuery({
@@ -12,30 +13,68 @@ const bigquery = new BigQuery({
   keyFilename: 'google.json'
 })
 
-async function run () {
-  try {
-    const { data } = await axios.get('http://192.168.3.175:8080/data/aircraft.json')
-    const rows = data.aircraft.map((input) => {
-      input.ts = Math.floor(data.now)
-      if (input.flight) input.flight = input.flight.trim()
-      return input
-    })
-    // Inserts data into a table
-    await bigquery
-    .dataset(datasetId)
-    .table(tableId)
-    .insert(rows)
+let batch = []
 
-    console.log(`Inserted ${rows.length} rows`)
+const client = new net.Socket()
+client.connect(PIPort, PIIP, function () {
+  console.log('Connected')
+})
+
+client.on('data', function (data) {
+  const rows = data.toString().split('\n')
+  const formatted = rows.filter((a) => {
+    return a !== ''
+  }).map((r) => {
+    const tsv = r.split('\t')
+    let output = {}
+    for (let i = 0; i < tsv.length; i = i + 2) {
+      if (tsv[i + 1]) {
+        if (tsv[i] === 'clock') tsv[i] = 'timestamp'
+        output[tsv[i]] = tsv[i + 1]
+      }
+    }
+
+    if (output.timestamp) output.timestamp = parseInt(output.timestamp)
+    if (output.ident) output.ident = output.ident.trim()
+    if (output.squawk) output.squawk = parseInt(output.squawk)
+    if (output.alt) output.alt = parseInt(output.alt)
+    if (output.speed) output.speed = parseInt(output.speed)
+    if (output.lat) output.lat = parseFloat(output.lat)
+    if (output.lon) output.lon = parseFloat(output.lon)
+    if (output.heading) output.heading = parseInt(output.heading)
+
+    return output
+  })
+  // await streamToBQ(formatted)
+  batch = batch.concat(formatted.filter((f) => {
+    return !!f.timestamp
+  }))
+})
+
+client.on('close', function () {
+  console.log('Connection closed')
+})
+
+async function streamToBQ () {
+  try {
+    const rows = batch.slice(0)
+    batch = []
+    await bigquery
+      .dataset(datasetId)
+      .table(tableId)
+      .insert(rows)
+
+    console.log(`${new Date()} - Inserted ${rows.length} rows`)
   } catch (err) {
     if (err && err.name === 'PartialFailureError') {
       if (err.errors && err.errors.length > 0) {
-        console.log('Insert errors:')
+        console.log(`${new Date()} - Insert errors:`)
         err.errors.forEach(err => console.error(err))
       }
     } else {
-      console.error('ERROR:', err)
+      console.error(`${new Date()} - ERROR:`, err)
     }
   }
 }
-setInterval(run, 5000)
+
+setInterval(streamToBQ, 5000)
